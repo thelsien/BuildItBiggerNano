6bdbc11e1dce5a657df25b488eb2eccb14492851
thelsien <789iuz@gmail.com>
2016-09-20 22:23:21 +0200

    Added connected test to test joke is coming from GCE.

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTextTest.java
@@ -0,0 +1,33 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class JokeTextTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testClickButtonAndJokeText() {
+        onView(withId(R.id.btn_joke)).perform(ViewActions.click());
+        onView(withId(R.id.tv_joke)).check(matches(not(withText(""))));
+    }
+}
